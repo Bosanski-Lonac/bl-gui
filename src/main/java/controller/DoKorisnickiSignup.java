70146ef2b061e11a89384f2ec5ba95d946740804
Andrej Gasic <andrej@gasic.rs>
2020-12-12 11:17:12 +0100

    Slanje mejla premesteno u korisnik-servis

--- src/main/java/controller/DoKorisnickiSignup.java
+++ src/main/java/controller/DoKorisnickiSignup.java
@@ -1,10 +1,5 @@
 package controller;
 
-import java.util.Properties;
-
-import javax.mail.*;
-import javax.mail.internet.*;
-
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -34,29 +29,7 @@ public class DoKorisnickiSignup implements EventHandler<ActionEvent> {
 		HttpEntity<KorisnikCUDto> request = new HttpEntity<>(korisnikCreateDto);
 		ResponseEntity<KorisnikDto> response = restTemplate.exchange(URL + KORISNIK_URL, HttpMethod.POST, request, KorisnikDto.class);
 		if(response.getStatusCode().equals(HttpStatus.CREATED)) {
-			String to=dialog.getEmail();
-			String from="bosanskilonacRN@gmail.com";
-			Properties properties=new Properties();
-			properties.put("mail.smtp.auth", "true");
-			properties.put("mail.smtp.starttls.enable", "true");
-			properties.put("mail.smtp.host", "smtp.gmail.com");
-			properties.put("mail.smtp.port", 587);
-			Session session=Session.getDefaultInstance(properties, new Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication("bosanskilonacRN@gmail.com", "bosanskilonac");
-				}
-			});
-			try {
-				MimeMessage message = new MimeMessage(session);
-				message.setFrom(new InternetAddress(from));
-				message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-				message.setSubject("Potvrda o registraciji");
-				message.setText("Uspesno ste se registrovali!");
-				Transport.send(message);
-		        System.out.println("Sent message successfully....");
-			} catch (MessagingException e) {
-				e.printStackTrace();
-			}
+			System.out.println("Uspeh");
 		}
 	}
 }
