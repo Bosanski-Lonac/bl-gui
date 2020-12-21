a99edcec20f0db4460d93b550440089ed73b5ba1
sbudimac <sbudimac@gmail.com>
2020-12-21 23:18:18 +0100

    Implementirane metode za dodavanje/brisanje letova/aviona

--- src/main/java/model/FlightOperator.java
+++ src/main/java/model/FlightOperator.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.math.BigDecimal;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -7,7 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import dto.AvionCUDto;
+import dto.AvionDto;
+import dto.LetCUDto;
 import dto.LetCriteriaDto;
+import dto.LetDto;
 import utility.BLURL;
 import wrapper.LetPageWrapper;
 
@@ -35,6 +41,42 @@ public class FlightOperator {
 		if(response.getStatusCode().equals(HttpStatus.OK)) {
 			return response.getBody();
 		} else {
+			throw new HttpClientErrorException(response.getStatusCode());
+		}
+	}
+	
+	public LetDto addFlight(String pocetnaDestinacija, String krajnjaDestinacija, Integer duzina, BigDecimal cena, Integer milje) {
+		LetCUDto letCreateDto=new LetCUDto(pocetnaDestinacija, krajnjaDestinacija, duzina, cena, milje);
+		HttpEntity<LetCUDto> request=new HttpEntity<>(letCreateDto);
+		ResponseEntity<LetDto> response=restTemplate.exchange(BLURL.SZL_URL + "/" + BLURL.LET_URL, HttpMethod.POST, request, LetDto.class);
+		if(response.getStatusCode().equals(HttpStatus.CREATED)) {
+			return response.getBody();
+		}else {
+			throw new HttpClientErrorException(response.getStatusCode());
+		}
+	}
+	
+	public void deleteFlight(Long flightId) {
+		ResponseEntity<Void> response=restTemplate.exchange(BLURL.SZL_URL + "/" + BLURL.LET_URL + "/" + flightId.toString(), HttpMethod.DELETE, null, Void.class);
+		if(!response.getStatusCode().equals(HttpStatus.OK)) {
+			throw new HttpClientErrorException(response.getStatusCode());
+		}
+	}
+	
+	public AvionDto addPlane(String naziv, Integer kapacitet) {
+		AvionCUDto avionCreateDto=new AvionCUDto(naziv, kapacitet);
+		HttpEntity<AvionCUDto> request=new HttpEntity<>(avionCreateDto);
+		ResponseEntity<AvionDto> response=restTemplate.exchange(BLURL.SZL_URL + "/" + BLURL.AVION_URL, HttpMethod.POST, request, AvionDto.class);
+		if(response.getStatusCode().equals(HttpStatus.CREATED)) {
+			return response.getBody();
+		}else {
+			throw new HttpClientErrorException(response.getStatusCode());
+		}
+	}
+	
+	public void deletePlane(Long planeId) {
+		ResponseEntity<Void> response=restTemplate.exchange(BLURL.SZL_URL + "/" + BLURL.AVION_URL + "/" + planeId.toString(), HttpMethod.DELETE, null, Void.class);
+		if(!response.getStatusCode().equals(HttpStatus.OK)) {
 			throw new HttpClientErrorException(response.getStatusCode());
 		}
 	}
