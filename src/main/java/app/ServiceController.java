1b77654e314a98ce13b35ad161cd8af159e44adc
Andrej Gasic <andrej@gasic.rs>
2021-05-15 21:54:38 +0200

    Odvoji pokretanje i namestanje servisa

--- src/main/java/app/ServiceController.java
+++ src/main/java/app/ServiceController.java
@@ -1,28 +1,40 @@
 package app;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 
 public class ServiceController {
     private ProcessBuilder[] processBuilders;
     private Process[] processes;
-    private String[] paths;
 
-    public ServiceController() throws IOException {
-        String direktorijum = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
-        direktorijum = direktorijum.substring(0, direktorijum.lastIndexOf("/") + 1);
-        System.out.println("DIREKTORIJUM: " + direktorijum);
+    public ServiceController() {
+        String direktorijumPutanja = System.getProperty ("user.dir");
+        File direktorijum = new File(direktorijumPutanja);
 
-        paths = new String[]{"eureka.jar", "gateway.jar", "ks.jar", "szl.jar", "szak.jar"};
+        String[] paths = new String[]{"eureka.jar", "ks.jar", "gateway.jar", "szl.jar", "szak.jar"};
         int len = paths.length;
         processBuilders = new ProcessBuilder[len];
         processes = new Process[len];
 
         for (int i = 0; i < len; i++) {
             processBuilders[i] = new ProcessBuilder("java", "-jar", paths[i]);
-            processBuilders[i].directory(new File(direktorijum));
+            processBuilders[i].directory(direktorijum);
+            File log = new File(direktorijum, "log" + i + ".txt");
+            processBuilders[i].redirectOutput(log);
+        }
+    }
+
+    public void start() throws IOException {
+        int len = processBuilders.length;
+        for(int i = 0; i < len; i++) {
             processes[i] = processBuilders[i].start();
-            System.out.println(processes[i].toString());
+            BufferedReader outputReader = new BufferedReader(new InputStreamReader(processes[i].getInputStream()));
+            String line;
+            do {
+                line = outputReader.readLine();
+            } while(!line.contains("Completed initialization in "))
+            outputReader.close();
         }
     }
 
