43bb9ec98a6ae6f3da5150828c1011bc8302b691
Andrej Gasic <andrej@gasic.rs>
2021-05-16 04:00:22 +0200

    Ukloni nepotrebnu granu

--- src/main/java/gui/MainView.java
+++ src/main/java/gui/MainView.java
@@ -37,9 +37,8 @@ public class MainView extends Stage {
 		dialog.showAndWait();
 		if (!dialog.getResult()) {
 			this.fireEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSE_REQUEST));
-		} else {
-			notificationManager.start();
 		}
+		notificationManager.start();
 	}
 	
 	public static MainView getInstance() {
